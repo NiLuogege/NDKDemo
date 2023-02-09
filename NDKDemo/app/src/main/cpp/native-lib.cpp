28281d3081ae16d9b4a358dbd7fe6636860310ad
luoChen <hank.luo@xianghuanji.com>
2023-02-09 10:51:37 +0800

    签名校验ok

--- NDKDemo/app/src/main/cpp/native-lib.cpp
+++ NDKDemo/app/src/main/cpp/native-lib.cpp
@@ -12,6 +12,7 @@ using namespace std;
 static char *EXTRA_SIGNATURE = "niluogege";
 //包名
 static char *PACKAGE_NAME = "com.niluogege.ndkdemo";
+static char *SIGNATURE_STR = "308202e4308201cc020101300d06092a864886f70d010105050030373116301406035504030c0d416e64726f69642044656275673110300e060355040a0c07416e64726f6964310b30090603550406130255533020170d3230303631383033353331325a180f32303530303631313033353331325a30373116301406035504030c0d416e64726f69642044656275673110300e060355040a0c07416e64726f6964310b300906035504061302555330820122300d06092a864886f70d01010105000382010f003082010a02820101008a6bbbde007cc4a3dd36bc48e6c18599f4327fcc502ea89a3fa9d3c06481dc01bb8aaf9cc1142bd27fe570ac6847743aadde02f0fc9ddc44c3b4e7777e8da1326d378febb77fc9ae40b4710ba83ca3adfd459217b3dfaf2e4d639a4775a262bc81e0851686438e02c1269c19467cbb4bd1f1f084bd996e13efbe56788927adb917d38a38802cd42ce313b1cd5db515c3f22f56a9ec3e9e70e5c1c100ea7a4ecab2190da5d588b85063825f0287ee283a680d97b468b9f8699c6e7f2d12f9c3cae60a2be5fbc15780ea51949d8f7e71ec46dc329765e7a1e65d91511a23d9de2c573d1010b870991375a0248685f0e5dbbf498309b6289ab2d0e87d1b2e5147890203010001300d06092a864886f70d01010505000382010100819878e7baf43c8c94f3e57a1259b7bffa8e28f1aa881c18e02f11c6721e9acd65859ad252bbe1348743b15107f76ad686cbfabee4602cbe6f5e2607b10e20eca6e20151a281657f9d75cfbfb73ff6580e3ff9aabaa390f01b76f849fe01c5d1fe3708da927111a7bd30b798dad7900e10a23de639edaa6a80d2d115fea40f79984481ce78b4c8f2e7a9395160c534f5c36baa4b82795bb0f36f389202951b0b65a56c89327df1c384dea57a393b182450485d0629e17eecb41baad411d8ddf3c2129353eb884ed9e7cfdf073b0dbe940412006b1d899f02282a827b07915c326cc41ed5a15d02de807c3e6e073b226aba4805e6acfa374b9eee4c199db7540b";
 
 //签名操作
 extern "C"
@@ -76,9 +77,9 @@ JNIEXPORT jboolean JNICALL
 Java_com_niluogege_ndkdemo_Signature_oauthVerify(JNIEnv *env, jobject thiz, jobject context) {
 
     //获取context的 jclass 对象
-    jclass j_cxt = env->GetObjectClass(context);
+    jclass j_class = env->GetObjectClass(context);
     //获取 getPackageName 的 methodid
-    jmethodID j_mid = env->GetMethodID(j_cxt, "getPackageName", "()Ljava/lang/String;");
+    jmethodID j_mid = env->GetMethodID(j_class, "getPackageName", "()Ljava/lang/String;");
     //调用 getPackageName 方法 ,并强转为 jstring
     jstring j_pack_name = (jstring) env->CallObjectMethod(context, j_mid);
     //转为c 字符串
@@ -89,6 +90,41 @@ Java_com_niluogege_ndkdemo_Signature_oauthVerify(JNIEnv *env, jobject thiz, jobj
         __android_log_print(ANDROID_LOG_ERROR, "JNI_TAG", "包名不一致");
         return false;
     }
+
+
+    //获取 getPackageManager methodid
+    j_mid = env->GetMethodID(j_class, "getPackageManager", "()Landroid/content/pm/PackageManager;");
+    // 调用 getPackageManager 获取到 PackageManager 对象
+    jobject pm = env->CallObjectMethod(context,j_mid);
+    //获取 PackageManager jclass对象
+    j_class = env->GetObjectClass(pm);
+    //获取 getPackageInfo methodid
+    j_mid = env->GetMethodID(j_class, "getPackageInfo", "(Ljava/lang/String;I)Landroid/content/pm/PackageInfo;");
+    //调用 getPackageInfo 获取到 PackageInfo 对象
+    //最后一个参数 64 为 PackageManager.GET_SIGNATURES
+    jobject pi = env->CallObjectMethod(pm,j_mid,j_pack_name,64);
+    // 获取 pi 的 jclass对象
+    j_class= env->GetObjectClass(pi);
+    //获取 signatures 的 属性id
+    jfieldID jfieldId = env->GetFieldID(j_class,"signatures","[Landroid/content/pm/Signature;");
+    //获取 jfieldId 的值
+    jobjectArray jobj_list = (jobjectArray)env->GetObjectField(pi,jfieldId);
+    //获取第0个值
+    jobject signature0 =env->GetObjectArrayElement(jobj_list,0);
+    //获取jclass 对象
+    j_class=env->GetObjectClass(signature0);
+    //获取 toCharsString 方法id
+    j_mid = env->GetMethodID(j_class,"toCharsString","()Ljava/lang/String;");
+    //调用 toCharsString 方法
+    jstring signature_jstr = (jstring)env->CallObjectMethod(signature0,j_mid);
+    //转为 c string
+    const char *signature_cstr=env->GetStringUTFChars(signature_jstr,0);
+
+    if (strcmp(signature_cstr,SIGNATURE_STR)!=0){
+        __android_log_print(ANDROID_LOG_ERROR, "JNI_TAG", "签名不一致");
+        return false;
+    }
+
 
     return true;
 }
--- NDKDemo/app/src/main/java/com/niluogege/ndkdemo/MainActivity.kt
+++ NDKDemo/app/src/main/java/com/niluogege/ndkdemo/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.niluogege.ndkdemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.niluogege.ndkdemo.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,9 +18,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val packageInfo: PackageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+        val s =packageInfo.signatures[0].toCharsString()
+
+        Log.e("签名为",s)
+
         // Example of a call to a native method
         binding.sampleText1.text = "权限校验结果：${Signature.oauthVerify(this)}"
         binding.sampleText.text = Signature.doSignature("sdfsdfsd111")
+        binding.sampleText2.text = "签名为：$s"
+
+
     }
 
 
